5a4565c2f3586026208de84823d6a2aac45bff5f
Phillip Boateng <cnolimit@gmail.com>
2019-07-12 20:01:29 +0100

    created twitter request

--- Main.go
+++ Main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"io"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
-)
 
-const searchAPI = "https://api.twitter.com/1.1/search/tweets.json"
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+)
 
 func main() {
 	const port = "3000"
@@ -22,17 +23,40 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("VALUE: %s\nTYPE: %T\n", r.URL.Path[:1], r.URL.Path[:1])
+	// fmt.Printf("VALUE: %s\nTYPE: %T\n", r.URL.Path[:1], r.URL.Path[:1])
 }
 
 func getTweets(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("QUERY: %s", r.URL.Query())
-	fmt.Fprint(w, "Here are your tweets!")
-	var res, err = http.Get(searchAPI)
+	const (
+		consumerKey    = "237yGfkFsctxG2YKMhP5lxQyS"
+		consumerSecret = "FkKAqXzlzQFLKcf1NIUgrsCduCic5ZDFu3jESDca2G4QkKdRTp"
+		accessToken    = "2820529160-RTQ0CFS8qDhlW8kDeP0I26N6Vl6FhdhSsPIXYKY"
+		accessSecret   = "Zkb495dO1bYGo0wvC7cfqy6qI5MIKqImMl5rlMRt0HzUa"
+	)
+	config := oauth1.NewConfig(consumerKey, consumerSecret)
+	token := oauth1.NewToken(accessToken, accessSecret)
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	// Twitter client
+	client := twitter.NewClient(httpClient)
+
+	tweets, resp, err := client.Timelines.HomeTimeline(&twitter.HomeTimelineParams{
+		Count: 20,
+	})
 
 	if err != nil {
 		panic("Issue with request")
 	}
 
-	fmt.Printf("response: %s\n", io.ReadAtLeast(res.Body))
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if resp.StatusCode == http.StatusBadRequest {
+		w.Write(body)
+	}
+
+	if resp.StatusCode == http.StatusOK {
+		t, _ := json.Marshal(tweets)
+		w.Write(t)
+	}
 }
